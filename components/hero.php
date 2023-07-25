aa043477250c8a37b9144d6abac609b26422309a
Adam Hills <adamhills91@gmail.com>
2023-07-25 09:29:38 +0100

    Playing with hero logo to stop mobile blurring

--- components/hero.php
+++ components/hero.php
@@ -13,13 +13,18 @@ $hero = $fields['hero'];
   <div class="gradient"></div>
   <div class="container">
     <div class="hero__content">
-      <?php if (is_front_page()) { ?>
+      <?php if (is_front_page()) {
+
+
+      ?>
         <!-- <h3><?= $hero['subtitle']; ?></h3>
         <h1><?= $hero['title']; ?></h1>
         <h4><?= $hero['tagline']; ?></h4>
         <?= $hero['copy']; ?> -->
 
-        <img class="hero-logo" src="<?= $hero['logo']['url'] ?>" alt="<?= $hero['logo']['alt'] ?>">
+        <!-- <img class="hero-logo" src="<?= $hero['logo']['url'] ?>" alt="<?= $hero['logo']['alt'] ?>"> -->
+
+        <?php echo wp_get_attachment_image($hero['logo']['id'], "full"); ?>
 
         <a href="<?= $hero['button']['link']; ?>" class="btn">
           <span?><?= $hero['button']['text']; ?></span>
