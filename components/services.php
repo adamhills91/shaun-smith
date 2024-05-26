05f20ea637d82409d0290d806c2a0c3bfb17e963
Adam Hills <adam@hamblyfreeman.com>
2024-05-26 18:56:35 +0100

    Fixing some accesibility issues, colour of text and link aria-labels

--- components/services.php
+++ components/services.php
@@ -18,7 +18,7 @@ $services = $fields['services'];
 
           <div class="service__card">
             <div class="service__card__img">
-              <a href="<?= $service['link']; ?>"><img src="<?= $service['image']['url'] ?>" alt="<?= $service['image']['alt'] ?>"></a>
+              <a href="<?= $service['link']; ?>" aria-label="<?= $service['title'] ?>"><img src="<?= $service['image']['url'] ?>" alt="<?= $service['image']['alt'] ?>"></a>
             </div>
             <h4> <a href="<?= $service['link']; ?>"><?= $service['title'] ?></a></h4>
             <?= $service['copy']; ?>
